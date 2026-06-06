5c790ef454770d550831c1f8bb6f0ca6ca8f10b0
raph-rc79 <raphaelrouetcoirier@gmail.com>
2026-06-06 18:56:45 -0300

    Comienzo de OBL2
    Archivos y carpetas nuevas, clases...

---
+++ OBL1/js/clases.js
@@ -0,0 +1,26 @@
+class Sistema {
+    constructor() {
+}
+class Influencer {
+    constructor(nombre, apellido, mail, comisión) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.mail = mail;
+        this.comisión = comisión;
+    }
+}
+class Artículo {
+    constructor(código, descripción, precio) {
+        this.código = código; 
+        this.descripción = descripción;
+        this.precio = precio;
+    }
+}
+class Venta {
+    constructor(nro_artículo, influencer, cantidad, medio) {
+        this.nro_artículo = nro_artículo;
+        this.influencer = influencer;
+        this.cantidad = cantidad;
+        this.medio = medio;
+    }
+}
