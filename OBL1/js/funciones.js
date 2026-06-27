3b1511990d07e7f98e13f3c7196d58561ceb07f4
raph-rc79 <raphaelrouetcoirier@gmail.com>
2026-06-27 12:46:38 -0300

    Correcciones + tabla influencer ordenada
    Se rompio todo a la mierda el formulario de influencer dice que ya existe el mail aunque no hayas ingresado datos

--- OBL1/js/funciones.js
+++ OBL1/js/funciones.js
@@ -49,13 +49,31 @@ window.addEventListener("load", () => {
   let hayInfluencer = false;
   let hayArtículo = false;
 
+  ok6.addEventListener("click", () => {
+    alert6.close();
+  })
+  ok7.addEventListener("click", () => {
+    alert7.close();
+  })
+  ok8.addEventListener("click", () => {
+    alert8.close();
+  })
+
   buttonopen.addEventListener("click", () => {
-    while (sistema.influencers.length === "" && sistema.artículos.length === "")
+    if (sistema.influencers.length === 0 && sistema.articulos.length === 0) {
       alert6.showModal();
-      ok6.addEventListener("click" , () => {
-        alert6.close
-      })
+    }
+    if (sistema.articulos.length === 0) {
+      alert7.showModal();
+    }
+    if (sistema.influencer.length === 0) {
+      alert8.showModal();
+    }
+    else {
+      dialog.showModal();
+    }
   });
+
   buttonclose.addEventListener("click", () => {
     dialog.close();
   });
@@ -66,12 +84,19 @@ window.addEventListener("load", () => {
   const n = document.getElementById("i1a");
   const m = document.getElementById("i1b");
   const c = document.getElementById("i1c");
+  const opcionesInfluencer = document.getElementById("i3b");
   const buttonadd = document.getElementById("a1");
   const alert1 = document.getElementById("d1a");
   const alert2 = document.getElementById("d1b");
   const ok1 = document.getElementById("Ok1");
   const ok2 = document.getElementById("Ok2");
-  let valor = 5;
+
+  ok1.addEventListener("click", () => {
+    alert1.close();
+  })
+  ok2.addEventListener("click", () => {
+    alert2.close();
+  })
 
   buttonadd.addEventListener("click", () => {
     if (
@@ -80,48 +105,27 @@ window.addEventListener("load", () => {
       c.value.trim() === ""
     ) {
       alert1.showModal();
-      ok1.addEventListener("click", () => {
-        alert1.close();
-      })
     }
     else {
       let existeMail = false;
       let pos = 0;
 
       while (pos < sistema.influencers.length && !existeMail) {
-        if (sistema.influencers[pos].mail === m.value) {
-
+        if (m.value === sistema.influencers[pos].mail) {
           existeMail = true;
         }
         pos++;
       }
       if (existeMail === true) {
         alert2.showModal();
-        ok2.addEventListener("click", () => {
-          alert2.close();
-        })
       }
       else {
         let influencer = new Influencer(n.value, m.value, Number(c.value));
         sistema.influencers.push(influencer);
-
-        valor++;
         let option = document.createElement("option");
         option.value = influencer.nombre;
         option.text = influencer.nombre;
-        document.getElementById("i3b").appendChild(option);
-
-        const table = document.getElementById("t1");
-        const row = table.insertRow();
-        const cell1 = row.insertCell();
-        const cell2 = row.insertCell();
-        const cell3 = row.insertCell();
-        cell1.textContent = influencer.nombre;
-        cell2.textContent = influencer.mail;
-        cell3.textContent = influencer.comision + "%";
-        document.getElementById("i1a").value = "";
-        document.getElementById("i1b").value = "";
-        document.getElementById("i1c").value = "";
+        opcionesInfluencer.appendChild(option);
       }
     }
   });
@@ -137,7 +141,13 @@ window.addEventListener("load", () => {
   const alert4 = document.getElementById("d2b");
   const ok3 = document.getElementById("Ok3")
   const ok4 = document.getElementById("Ok4");
-  let valor = 5;
+
+  ok3.addEventListener("click", () => {
+    alert3.close();
+  });
+  ok4.addEventListener("click", () => {
+    alert4.close();
+  })
 
   buttonadd.addEventListener("click", () => {
     if (
@@ -146,9 +156,6 @@ window.addEventListener("load", () => {
       p.value.trim() === ""
     ) {
       alert3.showModal();
-      ok3.addEventListener("click", () => {
-        alert3.close();
-      });
     }
     else {
       let existeCodigo = false;
@@ -162,15 +169,11 @@ window.addEventListener("load", () => {
       }
       if (existeCodigo === true) {
         alert4.showModal();
-        ok4.addEventListener("click", () => {
-          alert4.close();
-        })
       }
       else {
         let artículo = new Articulo(c.value, d.value, Number(p.value));
         sistema.articulos.push(artículo);
 
-        valor++;
         let option = document.createElement("option");
         option.value = artículo.codigo;
         option.text = artículo.codigo;
@@ -202,39 +205,53 @@ window.addEventListener("load", () => {
   const ok5 = document.getElementById("Ok5");
   const alert5 = document.getElementById("d4a");
 
+  ok5.addEventListener("click", () => {
+    alert5.close();
+  })
+
   buttonadd.addEventListener("click", () => {
     if (
       c.value.trim() === ""
     ) {
       alert5.showModal();
-      ok5.addEventListener("click", () => {
-        alert5.close();
-      })
     }
     else {
       let m = select.options[select.selectedIndex].text;
-      let venta = new Venta(
-        sistema.proximoNumeroVenta,
-        a.value,
-        i.value,
-        Number(c.value),
-        m,
-      );
+      let venta = new Venta(sistema.proximoNumeroVenta, a.value, i.value, Number(c.value), m);
       sistema.ventas.push(venta);
       sistema.proximoNumeroVenta = sistema.proximoNumeroVenta + 1;
-
-      const table = document.getElementById("t3");
-      const row = table.insertRow();
-      const cell1 = row.insertCell();
-      const cell2 = row.insertCell();
-      const cell3 = row.insertCell();
-      const cell4 = row.insertCell();
-      const cell5 = row.insertCell();
-      cell1.textContent = venta.numero;
-      cell2.textContent = venta.articulo;
-      cell3.textContent = venta.influencer;
-      cell4.textContent = venta.cantidad;
-      cell5.textContent = venta.medio;
     }
   });
 });
+
+//tabla ordenada infuencer
+window.addEventListener("load", () => {
+  const buttonOrder = document.getElementById("o1");
+  buttonOrder.addEventListener("click", () => {
+    ordenarInfluencers();
+    tablaInfluencers();
+  })
+
+})
+function ordenarInfluencers() {
+  sistema.influencers.sort(compararNombre);
+}
+function compararNombre(influencer1, influencer2) {
+  let resultado = influencer1.nombre.localeCompare(influencer2.nombre);
+  return resultado;
+}
+function tablaInfluencers() {
+  const tabla = document.getElementById("t3");
+  tabla.innerHTML = "";
+  for (let pos = 0; pos < sistema.influencers.length; pos++) {
+    const fila = document.createElement("tr");
+
+    for (let propiedad in sistema.influencers[pos]) {
+      const celda = document.createElement("td");
+      celda.innerHTML = sistema.influencers[pos][propiedad];
+      fila.appendChild(celda);
+    }
+    tabla.appendChild(fila);
+  }
+}
+
